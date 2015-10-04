5d7f423f82e0df9ad09508b635a5f2f49552fa1a
Steven Lundy <steven.lundy@gmail.com>
2015-10-03 22:12:56 -0700

    Write tests to check methods on todo.util existing

--- test/unit/utilities.js
+++ test/unit/utilities.js
@@ -6,6 +6,9 @@ describe('the todo.App', function() {
   describe('the todo object', function(){
 
     it('should have all the necessary methods', function(){
+      todo.util.trimTodoName.should.be.a('function');
+      todo.util.isValidTodoName.should.be.a('function');
+      todo.util.getUniqueId.should.be.a('function');
     });
   });
 });

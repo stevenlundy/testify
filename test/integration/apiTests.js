7a862cddc15cdddf7f242059c8d984c6d2027bfd
Steven Lundy <steven.lundy@gmail.com>
2015-10-03 23:46:01 -0700

    Write stubs for todo.api.sendRequest and todo.setup. Write JSONresponse data.

--- test/integration/apiTests.js
+++ test/integration/apiTests.js
@@ -1,6 +1,24 @@
 describe('API integration', function(){
   var server, setupStub, JSONresponse;
 
+  JSONresponse = {
+    todos: ['Write Tests', 'Profit']
+  };
+
+  before(function () {
+    server = sinon.stub(todo.api, 'sendRequest', function (options, callback) {
+      callback(null, JSONresponse);
+    });
+    setupStub = sinon.stub(todo, 'setup');
+  });
+
+  after(function () {
+    todo.api.restore();
+    todo.setup.restore();
+  });
+
   it('todo.setup receives an array of todos when todo.init is called', function () {
+    todo.init();
+    expect(setupStub.called).to.be(true);
   });
 });

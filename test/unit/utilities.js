8db9952f405c856f5c3c22502d35714cd49979c6
Steven Lundy <steven.lundy@gmail.com>
2015-10-03 22:20:32 -0700

    Write test to check functionality of trimTodoName

--- test/unit/utilities.js
+++ test/unit/utilities.js
@@ -3,7 +3,7 @@ var should = chai.should();
 var expect = chai.expect;
 
 describe('the todo.App', function() {
-  describe('the todo object', function(){
+  describe('the todo object', function() {
 
     it('should have all the necessary methods', function(){
       todo.util.trimTodoName.should.be.a('function');
@@ -14,4 +14,10 @@ describe('the todo.App', function() {
 });
 
 describe('the todo.util methods', function() {
+  describe('the trimTodoName funciton', function () {
+    it('should remove extra whitespace', function () {
+      expect(todo.util.trimTodoName('    Leading space')).to.equal('Leading space');
+      expect(todo.util.trimTodoName('Trailing space    ')).to.equal('Trailing space');
+    });
+  });
 });

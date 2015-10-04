24a0cec7b722729feaa0e9b95bd6606a316f599b
Steven Lundy <steven.lundy@gmail.com>
2015-10-04 09:52:54 -0700

    Write test to check that todo items are removed

--- test/e2e/todoFunctionality.js
+++ test/e2e/todoFunctionality.js
@@ -18,7 +18,7 @@ casper.test.begin('Adds todo items', function suite(test) {
     });
     test.assertExists('form', 'main form is found');
     this.fill('form', {
-        todo: 'testTodo'
+      todo: 'testTodo'
     }, true);
   });
 
@@ -26,7 +26,35 @@ casper.test.begin('Adds todo items', function suite(test) {
     var newTodos = this.evaluate(function() {
       return __utils__.findAll('.todo-item').length;
     });
-    test.assertEquals(oldTodos+1, newTodos);
+    test.assertEquals(newTodos, oldTodos+1);
+  });
+
+  casper.run(function() {
+    test.done();
+  });
+});
+
+casper.test.begin('Removes todo items', function suite(test) {
+  var oldTodos;
+  casper.start('http://localhost:3000/', function() {
+    this.fill('form', {
+      todo: 'testTodo'
+    }, true);
+  });
+
+  casper.then(function() {
+    oldTodos = this.evaluate(function() {
+      return __utils__.findAll('.todo-item').length;
+    });
+    test.assertExists('button.todo-remove', 'button to remove item is found');
+    this.click('button.todo-remove');
+  })
+
+  casper.then(function() {
+    var newTodos = this.evaluate(function() {
+      return __utils__.findAll('.todo-item').length;
+    });
+    test.assertEquals(newTodos, oldTodos-1);
   });
 
   casper.run(function() {
